0f22739bd77f0a8735d60cc57b7a9472e66ff527
Imon Chakraborty <chakraborty2005imon@gmail.com>
2025-03-30 18:06:18 +0530

    (REFACTOR): move app config to app.config.ts and delete app.json

---
+++ app.config.ts
@@ -0,0 +1,55 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: 'kaizen',
+    slug: 'kaizen',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/splash-icon.png',
+    scheme: 'myapp',
+    userInterfaceStyle: 'dark',
+    newArchEnabled: true,
+    platforms: ['ios', 'android'],
+    ios: {
+      supportsTablet: true,
+      userInterfaceStyle: 'dark',
+      bundleIdentifier: 'com.serenebrew.kaizen'
+    },
+    android: {
+      package: 'com.serenebrew.kaizen',
+      adaptiveIcon: {
+        foregroundImage: './assets/images/adaptive-icon.png',
+        backgroundColor: '#000000'
+      },
+      userInterfaceStyle: 'dark'
+    },
+    splash: {
+      image: './assets/images/splash-icon.png',
+      resizeMode: 'contain',
+      backgroundColor: '#000000',
+      imageWidth: 500,
+      imageHeight: 500
+    },
+    plugins: [
+      'expo-router',
+      [
+        'expo-splash-screen',
+        {
+          image: './assets/images/splash-icon.png',
+          imageResizeMode: 'contain',
+          backgroundColor: '#000000',
+          imageWidth: 500,
+          imageHeight: 500
+        }
+      ]
+    ],
+    extra: {
+      appwriteProjectId: process.env.APPWRITE_PROJECT_ID,
+      appwriteEndpoint: process.env.APPWRITE_ENDPOINT,
+    },
+    experiments: {
+      typedRoutes: true
+    }
+  }
+};
