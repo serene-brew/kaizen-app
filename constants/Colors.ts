333ff32e7aa7d573a377bc8bdbafef2ce89c8be0
Imon Chakraborty <chakraborty2005imon@gmail.com>
2025-03-30 13:47:41 +0530

    (ADD): Dark Theme

---
+++ constants/Colors.ts
@@ -0,0 +1,15 @@
+const Colors = {
+  dark: {
+    primary: '#007AFF',
+    background: '#000000',
+    secondaryBackground: '#1C1C1E',
+    text: '#FFFFFF',
+    secondaryText: '#8E8E93',
+    border: '#38383A',
+    tint: '#0A84FF',
+    tabIconDefault: '#6E6E73',
+    tabIconSelected: '#0A84FF',
+  }
+};
+
+export default Colors;
