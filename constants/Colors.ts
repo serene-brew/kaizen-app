50aaff68ec5911a10cdccab909ab9625f40fbfa1
Imon Chakraborty <chakraborty2005imon@gmail.com>
2025-03-31 17:52:27 +0530

    (ADD): More theme colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,7 +1,7 @@
 const Colors = {
   dark: {
     primary: '#007AFF',
-    background: '#000000',
+    background: '#161622',
     secondaryBackground: '#1C1C1E',
     text: '#FFFFFF',
     secondaryText: '#8E8E93',
@@ -9,6 +9,10 @@ const Colors = {
     tint: '#0A84FF',
     tabIconDefault: '#6E6E73',
     tabIconSelected: '#0A84FF',
+    buttonBackground: '#C23B22',  // #8B0000
+    inputBackground: '#232331',
+    inputBorder: '#2F2F3B',
+    placeholderText: '#666666',
   }
 };
 
