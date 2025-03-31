7567bf1a65416db296be50c6ebff11dcbe0628c1
Imon Chakraborty <chakraborty2005imon@gmail.com>
2025-03-31 17:51:50 +0530

    (ADD): Form field for sign in and sign up

---
+++ components/FormField.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import Colors from "../constants/Colors";
+
+interface FormFieldProps {
+  title: string;
+  value: string;
+  placeholder?: string;
+  handleChangeText: (text: string) => void;
+  otherStyles?: any;
+  keyboardType?: "default" | "email-address" | "numeric" | "phone-pad";
+  secureTextEntry?: boolean;
+}
+
+const FormField = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}: FormFieldProps) => {
+  return (
+    <View style={[styles.container, otherStyles]}>
+      <Text style={styles.title}>{title}</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          value={value}
+          placeholder={placeholder}
+          placeholderTextColor={Colors.dark.placeholderText}
+          onChangeText={handleChangeText}
+          {...props}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 8,
+  },
+  title: {
+    fontSize: 16,
+    color: Colors.dark.text,
+    fontWeight: '500',
+  },
+  inputContainer: {
+    width: '100%',
+    height: 64,
+    paddingHorizontal: 16,
+    backgroundColor: Colors.dark.inputBackground,
+    borderRadius: 16,
+    borderWidth: 2,
+    borderColor: Colors.dark.inputBorder,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    color: Colors.dark.text,
+    fontSize: 16,
+    fontWeight: '500',
+  },
+});
+
+export default FormField;
