570d98d9df948e43a736e116fb6fdf570aaca09e
Imon Chakraborty <chakraborty2005imon@gmail.com>
2025-04-06 19:04:39 +0530

    (ADD): SearchBar export to index for easier component access

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,4 @@ export { default as CustomButton } from './CustomButton';
 export { default as Loader } from './Loader';
 export { default as FormField } from './FormField';
 export { default as GoogleButton } from './GoogleButton';
+export { default as SearchBar } from './SearchBar';
