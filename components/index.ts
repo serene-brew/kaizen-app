0e12168f3da1af0faeb358fbf82ad8f329847964
Imon Chakraborty <chakraborty2005imon@gmail.com>
2025-04-25 20:33:58 +0530

    (REFACTOR): add OTPInput component to index exports for email verification flow

--- components/index.ts
+++ components/index.ts
@@ -3,3 +3,4 @@ export { default as Loader } from './Loader';
 export { default as FormField } from './FormField';
 export { default as GoogleButton } from './GoogleButton';
 export { default as SearchBar } from './SearchBar';
+export { default as OTPInput } from './OTPInput';
