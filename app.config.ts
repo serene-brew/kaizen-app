e3c0a068a97bd55ca30713213adf396d165d0909
Imon Chakraborty <chakraborty2005imon@gmail.com>
2025-05-16 13:02:43 +0530

    (REMOVE): unused splash screen configuration from app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -40,25 +40,8 @@ export default {
         }
       ]
     },
-    splash: {
-      image: './assets/images/splash-icon.png',
-      resizeMode: 'contain',
-      backgroundColor: '#161622',
-      imageWidth: 500,
-      imageHeight: 500
-    },
     plugins: [
       'expo-router',
-      [
-        'expo-splash-screen',
-        {
-          image: './assets/images/splash-icon.png',
-          imageResizeMode: 'contain',
-          backgroundColor: '#161622',
-          imageWidth: 500,
-          imageHeight: 500
-        }
-      ]
     ],
     extra: {
       appwriteProjectId: process.env.APPWRITE_PROJECT_ID,
