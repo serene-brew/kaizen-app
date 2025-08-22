318afe5bf01506dc804e92932dfe50b43cc0f493
Imon Chakraborty <chakraborty2005imon@gmail.com>
2025-08-22 19:37:04 +0530

    (ADD): export for CustomAlert and its associated alert functions

--- components/index.ts
+++ components/index.ts
@@ -4,3 +4,4 @@ export { default as FormField } from './FormField';
 export { default as GoogleButton } from './GoogleButton';
 export { default as SearchBar } from './SearchBar';
 export { default as OTPInput } from './OTPInput';
+export { default as CustomAlert, showCustomAlert, showSuccessAlert, showErrorAlert, showWarningAlert, showConfirmAlert } from './CustomAlert';
