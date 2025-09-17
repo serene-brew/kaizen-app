e1933b4d57bf7e0ca1b31cc3564bfadcb902fb0b
Imon Chakraborty <chakraborty2005imon@gmail.com>
2025-09-17 21:54:22 +0530

    (BUMP): update app version to 1.1.1 in more component

--- app/(tabs)/more.tsx
+++ app/(tabs)/more.tsx
@@ -50,7 +50,7 @@ interface MenuItemProps {
 // Static assets and constants
 const AppIcon = require('../../assets/images/icon.png');
 const GITHUB_URL = "https://github.com/serene-brew/kaizen-app";
-const APP_VERSION = "1.1.0";
+const APP_VERSION = "1.1.1";
 const DEFAULT_AVATAR_ICON = 'account-circle';
 
 /**
