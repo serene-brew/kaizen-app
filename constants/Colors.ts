219952652f3f691f994b6401a8aa7e309dc2a68e
Imon Chakraborty <chakraborty2005imon@gmail.com>
2026-01-06 19:26:48 +0530

    (UPDATE): adjust secondaryBackground and inputBackground colors for consistency

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -2,7 +2,7 @@ const Colors = {
   dark: {
     primary: '#007AFF',
     background: '#161622',
-    secondaryBackground: '#1C1C1E',
+    secondaryBackground: '#232331',
     text: '#FFFFFF',
     secondaryText: '#8E8E93',
     border: '#38383A',
@@ -10,7 +10,7 @@ const Colors = {
     tabIconDefault: '#6E6E73',
     tabIconSelected: '#0A84FF',
     buttonBackground: '#C23B22',  // #8B0000
-    inputBackground: '#232331',
+    inputBackground: '#2C2C3E',
     inputBorder: '#2F2F3B',
     placeholderText: '#666666',
   }
